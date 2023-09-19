a1c93d691e0252c41c24c9e545a5acf861d8074a
parnasha <parnasha.s@digitaledge360.in>
2023-09-19 11:31:29 +0530

    graphql connection done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,128 +4,184 @@ import Image from "next/image";
 import cover_picture from "../../assets/cover_picture.jpg";
 import Link from "next/link";
 import "./page.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "http://localhost:5000/graphql",
+    cache: new InMemoryCache(),
+});
+
+export async function getUsers() {
+    const query = gql`
+        query {
+            getUsers {
+                name
+                age
+            }
+        }
+    `;
+
+    // const { company } = await client.request(query, { id });
+
+    const { data } = await client.query({ query });
+
+    console.log(data);
+}
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [usernamError, setUsernameError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [usernamError, setUsernameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    console.log("parnasha");
+    useEffect(() => {
+        const fetchData = async () => {
+            await getUsers();
+        };
+        console.log("hereee");
+        fetchData();
+    }, []);
 
-  const handleClickLogin = () => {
-    console.log("clicked", username, password);
-    if (!username) {
-      setUsernameError("Username is required");
-    } else {
-      setUsernameError("");
-    }
-    if (!password) {
-      setPasswordError("Password is required");
-    } else {
-      setPasswordError("");
-    }
-  };
-  const handleRegisterNow = () => {};
-  return (
-    <>
-      <div
-        style={{ backgroundColor: "#3D259B", height: "100vh", width: "100vw" }}
-        className="flex items-center justify-center"
-      >
-        <div
-          style={{
-            backgroundColor: "#ffffff",
-            height: "80vh",
-            width: "80%",
-          }}
-          className="mx-auto rounded-md"
-        >
-          <div className="grid grid-cols-3 gap-4 ">
-            <div className="col-span-2 flex items-center justify-center">
-              <div
-                style={{ width: "500px", height: "80vh" }}
-                className="mx-auto flex items-center justify-center"
-              >
-                <Image
-                  src={cover_picture}
-                  alt="cover"
-                  // width="50px"
-                  // height="60px"
-                />
-              </div>
-            </div>
-            <div className="col-span-1 flex items-center justify-center">
-              <div className="grid grid-rows-4">
+    const handleClickLogin = () => {
+        console.log("clicked", username, password);
+        if (!username) {
+            setUsernameError("Username is required");
+        } else {
+            setUsernameError("");
+        }
+        if (!password) {
+            setPasswordError("Password is required");
+        } else {
+            setPasswordError("");
+        }
+    };
+    const handleRegisterNow = () => {};
+    return (
+        <>
+            <ApolloProvider client={client}>
                 <div
-                  className="row-span-1 text-3xl font-bold mb-5"
-                  style={{ fontFamily: "font-family: 'Kalam', cursive;" }}
+                    style={{
+                        backgroundColor: "#3D259B",
+                        height: "100vh",
+                        width: "100vw",
+                    }}
+                    className="flex items-center justify-center"
                 >
-                  <h1 style={{ color: "#4022C9" }}>Login</h1>
+                    <div
+                        style={{
+                            backgroundColor: "#ffffff",
+                            height: "80vh",
+                            width: "80%",
+                        }}
+                        className="mx-auto rounded-md"
+                    >
+                        <div className="grid grid-cols-3 gap-4 ">
+                            <div className="col-span-2 flex items-center justify-center">
+                                <div
+                                    style={{ width: "500px", height: "80vh" }}
+                                    className="mx-auto flex items-center justify-center"
+                                >
+                                    <Image
+                                        src={cover_picture}
+                                        alt="cover"
+                                        // width="50px"
+                                        // height="60px"
+                                    />
+                                </div>
+                            </div>
+                            <div className="col-span-1 flex items-center justify-center">
+                                <div className="grid grid-rows-4">
+                                    <div
+                                        className="row-span-1 text-3xl font-bold mb-5"
+                                        style={{
+                                            fontFamily:
+                                                "font-family: 'Kalam', cursive;",
+                                        }}
+                                    >
+                                        <h1 style={{ color: "#4022C9" }}>
+                                            Login
+                                        </h1>
+                                    </div>
+                                    <div className="row-span-1 text-sm mt-4">
+                                        <div style={{ color: "#a6a6a6" }}>
+                                            Username*
+                                        </div>
+                                        <div>
+                                            <input
+                                                className="inputBox"
+                                                type="text"
+                                                placeholder="John doe"
+                                                onChange={(e) => {
+                                                    setUsername(e.target.value);
+                                                }}
+                                                value={username}
+                                            />
+                                            <div className="text-red-400 text-sm">
+                                                {usernamError}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row-span-1 mt-3">
+                                        <div style={{ color: "#a6a6a6" }}>
+                                            Password*
+                                        </div>
+                                        <div>
+                                            <input
+                                                className="inputBox"
+                                                type="password"
+                                                placeholder="*********"
+                                                onChange={(e) => {
+                                                    setPassword(e.target.value);
+                                                }}
+                                                value={password}
+                                            />
+                                            <div className="text-red-400 text-sm">
+                                                {passwordError}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row-span-1">
+                                        <div
+                                            style={{
+                                                height: "30px",
+                                                width: "225px",
+                                                backgroundImage:
+                                                    "linear-gradient(to right, #643ED9 , #3A1EC7)",
+                                            }}
+                                            className="text-center text-white rounded-sm mt-10 cursor-pointer"
+                                            onClick={handleClickLogin}
+                                        >
+                                            Login
+                                        </div>
+                                        <div
+                                            style={{
+                                                color: "#4022C9",
+                                                fontSize: "12px",
+                                                marginTop: "5px",
+                                            }}
+                                            className=" text-center cursor-pointer"
+                                            onClick={handleRegisterNow}
+                                        >
+                                            <Link href="/register">
+                                                Don't have an account?Register
+                                                now
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="row-span-1 text-sm mt-4">
-                  <div style={{ color: "#a6a6a6" }}>Username*</div>
-                  <div>
-                    <input
-                      className="inputBox"
-                      type="text"
-                      placeholder="John doe"
-                      onChange={(e) => {
-                        setUsername(e.target.value);
-                      }}
-                      value={username}
-                    />
-                    <div className="text-red-400 text-sm">{usernamError}</div>
-                  </div>
-                </div>
-                <div className="row-span-1 mt-3">
-                  <div style={{ color: "#a6a6a6" }}>Password*</div>
-                  <div>
-                    <input
-                      className="inputBox"
-                      type="password"
-                      placeholder="*********"
-                      onChange={(e) => {
-                        setPassword(e.target.value);
-                      }}
-                      value={password}
-                    />
-                    <div className="text-red-400 text-sm">{passwordError}</div>
-                  </div>
-                </div>
-                <div className="row-span-1">
-                  <div
-                    style={{
-                      height: "30px",
-                      width: "225px",
-                      backgroundImage:
-                        "linear-gradient(to right, #643ED9 , #3A1EC7)",
-                    }}
-                    className="text-center text-white rounded-sm mt-10 cursor-pointer"
-                    onClick={handleClickLogin}
-                  >
-                    Login
-                  </div>
-                  <div
-                    style={{
-                      color: "#4022C9",
-                      fontSize: "12px",
-                      marginTop: "5px",
-                    }}
-                    className=" text-center cursor-pointer"
-                    onClick={handleRegisterNow}
-                  >
-                    <Link href="/register">
-                      Don't have an account?Register now
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+            </ApolloProvider>
+        </>
+    );
 };
 
 export default Login;
