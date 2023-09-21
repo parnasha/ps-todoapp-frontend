3ecd3ac6cbdb334e3c37ba7eac0bfda932df09e1
parnasha <parnasha.s@digitaledge360.in>
2023-09-21 22:06:21 +0530

    check box functionality added

--- src/component/card.tsx
+++ src/component/card.tsx
@@ -6,120 +6,130 @@ import { AiOutlineCheckCircle } from "react-icons/ai";
 import "./card.css";
 
 interface CardProps {
-    id: string;
-    title: string;
-    description: string;
-    onDeleteClick: (id: string) => void;
-    onSaveEdit: (
-        id: string,
-        editedTitle: string,
-        editedDescription: string
-    ) => void;
+  id: string;
+  title: string;
+  description: string;
+  isDone: boolean;
+  onDeleteClick: (id: string) => void;
+  onSaveEdit: (
+    id: string,
+    editedTitle: string,
+    editedDescription: string
+  ) => void;
+  onCheckTask: (id: string, isDone: boolean) => void;
 }
 
 const Card: React.FC<CardProps> = ({
-    id,
-    title,
-    description,
-    onDeleteClick,
-    onSaveEdit,
+  id,
+  title,
+  description,
+  isDone,
+  onDeleteClick,
+  onSaveEdit,
+  onCheckTask,
 }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [editedTitle, setEditedTitle] = useState(title);
-    const [editedDescription, setEditedDescription] = useState(description);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+  const [editedDescription, setEditedDescription] = useState(description);
+  const [isChecked, setIsChecked] = useState(isDone);
 
-    const handleEditing = () => {
-        setIsEditing((state) => !state);
-    };
+  const handleEditing = () => {
+    setIsEditing((state) => !state);
+  };
 
-    const handleSaveClick = () => {
-        onSaveEdit(id, editedTitle, editedDescription);
-        setIsEditing(false); // Disable editing mode after saving
-    };
+  const handleSaveClick = () => {
+    onSaveEdit(id, editedTitle, editedDescription);
+    setIsEditing(false); // Disable editing mode after saving
+  };
+  const handleCheckboxClick = () => {
+    setIsChecked(!isChecked);
+    // console.log(id)
+    // Toggle the isChecked state and update isDone prop in the parent
+    onCheckTask(id, !isChecked);
+    // You can also pass the updated isDone value to the parent component here if needed
+  };
 
-    return (
-        <div className="cardWrapper">
-            <div className="grid grid-cols-5 gap-1">
-                <div className="flex items-center justify-center">
-                    <input type="checkbox" />
-                </div>
-                <div className="col-span-3">
-                    <div className="grid grid-rows-2 grid-flow-col">
-                        <div className="col-span-1">
-                            <div className="text-xl font-semibold">
-                                {isEditing ? (
-                                    <input
-                                        type="text"
-                                        value={editedTitle}
-                                        onChange={(e) =>
-                                            setEditedTitle(e.target.value)
-                                        }
-                                    />
-                                ) : (
-                                    <h1 style={{ color: "#4022C9" }}>
-                                        {title}
-                                    </h1>
-                                )}
-                            </div>
-                        </div>
-                        <div className="col-span-1">
-                            <div className="text-md">
-                                {isEditing ? (
-                                    <input
-                                        type="text"
-                                        value={editedDescription}
-                                        onChange={(e) =>
-                                            setEditedDescription(e.target.value)
-                                        }
-                                    />
-                                ) : (
-                                    <h1 style={{ color: "#9a989e" }}>
-                                        {description}
-                                    </h1>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className="grid grid-cols-4 py-5">
-                        <div className="col-span-2">
-                            <div className="cursor-pointer">
-                                {isEditing ? (
-                                    <AiOutlineCheckCircle
-                                        style={{
-                                            fontSize: "24px",
-                                            color: "#00cc00", // Save icon color
-                                        }}
-                                        onClick={handleSaveClick}
-                                    />
-                                ) : (
-                                    <IoMdCreate
-                                        style={{
-                                            fontSize: "24px",
-                                            color: "#908e94",
-                                        }}
-                                        onClick={handleEditing}
-                                    />
-                                )}
-                            </div>
-                        </div>
-                        <div className="col-span-2">
-                            <div className="cursor-pointer">
-                                <IoMdTrash
-                                    style={{
-                                        fontSize: "24px",
-                                        color: "#ff7373",
-                                    }}
-                                    onClick={() => onDeleteClick(id)}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="cardWrapper">
+      <div className="grid grid-cols-5 gap-1">
+        <div className="flex items-center justify-center">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxClick}
+          />
         </div>
-    );
+        <div className="col-span-3">
+          <div className="grid grid-rows-2 grid-flow-col">
+            <div className="col-span-1">
+              <div className="text-xl font-semibold">
+                {isEditing ? (
+                  <input
+                    type="text"
+                    value={editedTitle}
+                    onChange={(e) => setEditedTitle(e.target.value)}
+                    className="editInput "
+                  />
+                ) : (
+                  <h1 style={{ color: "#4022C9" }}>{title}</h1>
+                )}
+              </div>
+            </div>
+            <div className="col-span-1">
+              <div className="text-md">
+                {isEditing ? (
+                  <input
+                    type="text"
+                    value={editedDescription}
+                    onChange={(e) => setEditedDescription(e.target.value)}
+                    className="editInput "
+                  />
+                ) : (
+                  <h1 style={{ color: "#9a989e" }}>{description}</h1>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="grid grid-cols-4 py-5">
+            <div className="col-span-2">
+              <div className="cursor-pointer">
+                {isEditing ? (
+                  <AiOutlineCheckCircle
+                    style={{
+                      fontSize: "24px",
+                      color: "#00cc00", // Save icon color
+                    }}
+                    onClick={handleSaveClick}
+                  />
+                ) : (
+                  <IoMdCreate
+                    style={{
+                      fontSize: "24px",
+                      color: "#908e94",
+                    }}
+                    onClick={handleEditing}
+                  />
+                )}
+              </div>
+            </div>
+            <div className="col-span-2">
+              <div className="cursor-pointer">
+                <IoMdTrash
+                  style={{
+                    fontSize: "24px",
+                    color: "#ff7373",
+                  }}
+                  onClick={() => onDeleteClick(id)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,276 +1,265 @@
 // Import necessary libraries and modules
 import { getToken } from "../token";
 import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    gql,
-    ApolloLink,
-    createHttpLink,
-    concat,
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  ApolloLink,
+  createHttpLink,
+  concat,
 } from "@apollo/client";
 // Create an HTTP link to your GraphQL server
 const httpLink = createHttpLink({ uri: "http://localhost:5000/graphql" });
 // Create an ApolloLink to handle authentication by adding an Authorization header
 const authLink = new ApolloLink((operation, forward) => {
-    const accessToken = getToken();
-    if (accessToken) {
-        operation.setContext({
-            headers: { Authorization: `Bearer ${accessToken}` },
-        });
-    }
-    return forward(operation);
+  const accessToken = getToken();
+  if (accessToken) {
+    operation.setContext({
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+  }
+  return forward(operation);
 });
 // Initialize the Apollo Client
 const client = new ApolloClient({
-    // Combine authLink and httpLink
-    link: concat(authLink, httpLink),
-    cache: new InMemoryCache({ addTypename: true }),
+  // Combine authLink and httpLink
+  link: concat(authLink, httpLink),
+  cache: new InMemoryCache({ addTypename: true }),
 });
 // Function to get a list of users
 export async function getUsers() {
-    const query = gql`
-        query {
-            getUsers {
-                name
-                age
-                dob
-            }
-        }
-    `;
+  const query = gql`
+    query {
+      getUsers {
+        name
+        age
+        dob
+      }
+    }
+  `;
 
-    const { data } = await client.query({ query });
+  const { data } = await client.query({ query });
 
-    console.log(data);
+  console.log(data);
 }
 
 // Function to register a new user
 export async function register(
-    name: string,
-    email: string,
-    password: string,
-    gender: string,
-    dob: string
+  name: string,
+  email: string,
+  password: string,
+  gender: string,
+  dob: string
 ) {
-    const mutation = gql`
-        mutation (
-            $name: String!
-            $email: String!
-            $dob: Date!
-            $gender: String!
-            $password: String!
-        ) {
-            registerUser(
-                name: $name
-                email: $email
-                dob: $dob
-                gender: $gender
-                password: $password
-            ) {
-                token
-            }
-        }
-    `;
-    const { data } = await client.mutate({
-        mutation,
-        variables: {
-            name: name,
-            email: email,
-            dob: dob,
-            gender: gender,
-            password: password,
-        },
-    });
+  const mutation = gql`
+    mutation (
+      $name: String!
+      $email: String!
+      $dob: Date!
+      $gender: String!
+      $password: String!
+    ) {
+      registerUser(
+        name: $name
+        email: $email
+        dob: $dob
+        gender: $gender
+        password: $password
+      ) {
+        token
+      }
+    }
+  `;
+  const { data } = await client.mutate({
+    mutation,
+    variables: {
+      name: name,
+      email: email,
+      dob: dob,
+      gender: gender,
+      password: password,
+    },
+  });
 
-    return data;
+  return data;
 }
 // Function to log in a user
 export async function login(email: string, password: string) {
-    try {
-        const mutation = gql`
-            mutation ($email: String!, $password: String!) {
-                loginUser(email: $email, password: $password) {
-                    token
-                }
-            }
-        `;
-        const { data } = await client.mutate({
-            mutation,
-            variables: {
-                email: email,
-                password: password,
-            },
-        });
+  try {
+    const mutation = gql`
+      mutation ($email: String!, $password: String!) {
+        loginUser(email: $email, password: $password) {
+          token
+        }
+      }
+    `;
+    const { data } = await client.mutate({
+      mutation,
+      variables: {
+        email: email,
+        password: password,
+      },
+    });
 
-        console.log(data);
+    console.log(data);
 
-        return data;
-    } catch (error) {
-        return error;
-    }
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
 // Function for adding a new task
 export async function addTask(title: string, description: string) {
-    try {
-        const mutation = gql`
-            mutation ($title: String!, $description: String!) {
-                createTodo(title: $title, description: $description) {
-                    title
-                    description
-                    isDone
-                }
+  try {
+    const mutation = gql`
+      mutation ($title: String!, $description: String!) {
+        createTodo(title: $title, description: $description) {
+          title
+          description
+          isDone
+        }
+      }
+    `;
+    const { data } = await client.mutate({
+      mutation,
+      variables: {
+        title: title,
+        description: description,
+      },
+      refetchQueries: [
+        {
+          query: gql`
+            query GetTodosByUser {
+              getTodosByUser {
+                id
+                title
+                description
+                isDone
+              }
             }
-        `;
-        const { data } = await client.mutate({
-            mutation,
-            variables: {
-                title: title,
-                description: description,
-            },
-            refetchQueries: [
-                {
-                    query: gql`
-                        query GetTodosByUser {
-                            getTodosByUser {
-                                id
-                                title
-                                description
-                                isDone
-                            }
-                        }
-                    `,
-                },
-            ],
-        });
+          `,
+        },
+      ],
+    });
 
-        return data;
-    } catch (error) {}
+    return data;
+  } catch (error) {}
 }
 // Function for getting a list of todos
 export async function getTodo() {
-    try {
-        const query = gql`
-            query GetTodosByUser {
-                getTodosByUser {
-                    id
-                    title
-                    description
-                    isDone
-                }
-            }
-        `;
-        const { data } = await client.query({ query });
-        return data.getTodosByUser;
-    } catch (error) {
-        console.log("getTodo error", error);
-    }
+  try {
+    const query = gql`
+      query GetTodosByUser {
+        getTodosByUser {
+          id
+          title
+          description
+          isDone
+        }
+      }
+    `;
+    const { data } = await client.query({ query });
+    return data.getTodosByUser;
+  } catch (error) {
+    console.log("getTodo error", error);
+  }
 }
 // Function for deleting a task
 export async function deleteTask(id: string) {
-    try {
-        const mutation = gql`
-            mutation ($todoId: ID!) {
-                deleteTodo(todoId: $todoId)
+  try {
+    const mutation = gql`
+      mutation ($todoId: ID!) {
+        deleteTodo(todoId: $todoId)
+      }
+    `;
+    const { data } = await client.mutate({
+      mutation,
+      variables: {
+        todoId: id,
+      },
+      refetchQueries: [
+        {
+          query: gql`
+            query GetTodosByUser {
+              getTodosByUser {
+                id
+                title
+                description
+                isDone
+              }
             }
-        `;
-        const { data } = await client.mutate({
-            mutation,
-            variables: {
-                todoId: id,
-            },
-            refetchQueries: [
-                {
-                    query: gql`
-                        query GetTodosByUser {
-                            getTodosByUser {
-                                id
-                                title
-                                description
-                                isDone
-                            }
-                        }
-                    `,
-                },
-            ],
-        });
+          `,
+        },
+      ],
+    });
 
-        console.log(data);
+    console.log(data);
 
-        return data;
-    } catch (error) {
-        return error;
-    }
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
 // Function for editing a task
 export async function updateTask(
-    id: string,
-    title: string,
-    description: string
+  id: string,
+  title: string,
+  description: string
 ) {
-    try {
-        console.log(id, title, description);
-        const mutation = gql`
-            mutation ($id: ID!, $title: String, $description: String) {
-                editTodo(_id: $id, title: $title, description: $description) {
-                    id
-                    title
-                    description
-                }
-            }
-        `;
+  try {
+    console.log(id, title, description);
+    const mutation = gql`
+      mutation ($id: ID!, $title: String, $description: String) {
+        editTodo(_id: $id, title: $title, description: $description) {
+          id
+          title
+          description
+        }
+      }
+    `;
 
-        const { data } = await client.mutate({
-            mutation,
-            variables: {
-                id: id,
-                title: title,
-                description: description,
-            },
-        });
+    const { data } = await client.mutate({
+      mutation,
+      variables: {
+        id: id,
+        title: title,
+        description: description,
+      },
+    });
 
-        console.log(data);
+    console.log(data);
 
-        return data;
-    } catch (error) {
-        console.log(error);
-        return error;
-    }
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
 // Function for checking a task
 export async function checkTask(id: string, isDone: Boolean) {
-    try {
-        const mutation = gql`
-            mutation EditTodo($id: ID!, $isDone: Boolean) {
-                editTodo(_id: $id, isDone: $isDone) {
-                    id
-                    isDone
-                }
-            }
-        `;
-        const { data } = await client.mutate({
-            mutation,
-            variables: {
-                todoId: id,
-                isDone: isDone,
-            },
-            refetchQueries: [
-                {
-                    query: gql`
-                        query GetTodosByUser {
-                            getTodosByUser {
-                                id
-                                isDone
-                            }
-                        }
-                    `,
-                },
-            ],
-        });
+  try {
+    const mutation = gql`
+      mutation ($id: ID!, $isDone: Boolean) {
+        editTodo(_id: $id, isDone: $isDone) {
+          id
+          isDone
+        }
+      }
+    `;
+    const { data } = await client.mutate({
+      mutation,
+      variables: {
+        id: id,
+        isDone: isDone,
+      },
+    });
 
-        console.log(data);
+    console.log(data);
 
-        return data;
-    } catch (error) {
-        return error;
-    }
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
